9c74bd53a4a73ddf5737a273893fec901f510316
Spark Knight <jianji2019@foxmail.com>
2024-05-19 20:38:06 +0800

    FEAT: Change the code style.

--- assignments/project2/src/tools/pendulum.py
+++ assignments/project2/src/tools/pendulum.py
@@ -34,15 +34,8 @@ class Pendulum:
 
         self.Q = Q.copy()
         self.R = R.copy()
-        print('A: ', self.A)
-        print('B: ', self.B)
-        print('shape of A: ', self.A.shape)
-        print('shape of B: ', self.B.shape)
-        print('shape of Q: ', self.Q.shape)
-        print('shape of R: ', self.R.shape)
         self.K = None
         self.updataK(epochs = epochs)
-        print('SHAPE of K: ', self.K.shape)
         self.x = x.copy()
         self.u = u.copy()
 
@@ -75,19 +68,11 @@ class Pendulum:
         # self.K = control.dlqr(self.A, self.B, self.Q, self.R)[0]
         self.K = lqr.getDiscreteKN(self.A, self.B, self.Q, self.R, epochs, 4, 1)
 
-    # def free_falling(self):
-    #     self.x = self.A@self.x
-    #     self.zs.append(self.x[0][0])
-    #     self.thetas.append(float(np.pi)-self.x[1][0])
-    #     return self.x.copy(), self.u.copy()
-
     def step_in(self):
         '''
         update the state of the pendulum, in one step. The return value will be the new state of the pendulum.
         :return: state matrix of x, and input u
         '''
-        # print('shape of K: ', self.K.shape)
-        # print('shape of u: ', self.u.shape)
         self.u = -self.K @ self.x
         self.x = self.A @ self.x + self.B @ self.u
         self.zs.append(self.x[0][0])
