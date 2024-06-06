e4a64874bc706ae5056ef24819d4e2b9a62293a5
Spark Knight <jianji2019@foxmail.com>
2024-06-06 20:59:10 +0800

    FEAT: update template for q2.

--- assignments/finpro/RL_train_pendulum/pendulum_project_demo.py
+++ assignments/finpro/RL_train_pendulum/pendulum_project_demo.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+import os
+import torch.optim as optim
 import numpy as np
-import gymnasium as gym
 import torch
 import torch.nn as nn
 from torch.distributions.normal import Normal
+import mujoco
+import mujoco.viewer
+import time
 
 class Agent:
     """Agent that learns to solve the Inverted Pendulum task using a policy gradient algorithm.
@@ -68,24 +72,64 @@ class Policy_Network(nn.Module):
         # Implement the prediction logic here
         return torch.tensor(0.0), torch.tensor(1.0)  # Example placeholders
 
+def init_mujoco(model_path):
+    mujoco_model = mujoco.MjModel.from_xml_path(model_path)
+    mujoco_data = mujoco.MjData(mujoco_model)
+    return mujoco_model, mujoco_data
+
 if __name__ == "__main__":
-    env = gym.make("InvertedPendulum-v4", render_mode="human")  # Initialize the environment
-    # env = gym.make("InvertedDoublePendulum-v4")
-    wrapped_env = gym.wrappers.RecordEpisodeStatistics(env, 50)  # Wrap the environment to record statistics
-    
-    obs_space_dims = env.observation_space.shape[0]  # Dimension of the observation space
-    action_space_dims = env.action_space.shape[0]  # Dimension of the action space
-    agent = Agent(obs_space_dims, action_space_dims)  # Instantiate the agent
-    
-    total_num_episodes = int(5e3)  # Total number of episodes
-    
-    # Simulation main loop
-    for episode in range(total_num_episodes):
-        obs, info = wrapped_env.reset()  # Reset the environment at the start of each episode
-        done = False
-        while not done:
-            action = agent.sample_action(obs)  # Sample an action based on the current observation
-            obs, reward, terminated, truncated, _ = wrapped_env.step(action)  # Take the action in the environment
-            done = terminated or truncated  # Check if the episode has terminated
-        # The collection of rewards and log probabilities should happen within the loop.
-        # agent.update(rewards, log_probs)  # Update the policy based on the episode's experience
+    xml_path = "inverted_pendulum.xml"
+
+    model, data = init_mujoco(xml_path)
+
+    obs_space_dims = model.nq
+    action_space_dims = model.nu
+    agent = Agent(obs_space_dims, action_space_dims)
+
+    # create viewer
+    with mujoco.viewer.launch_passive(model, data, show_left_ui=False, show_right_ui=False) as viewer:
+        total_num_episodes = int(3e4)
+        episode = 0
+        while viewer.is_running() and episode < total_num_episodes:
+            rewards = []
+            log_probs = []
+            states = []
+            done = False
+            data.time = 0
+
+            # reset to initial state
+            mujoco.mj_resetData(model, data)
+
+            # TODO: Here, you need to add noise to the model manully!
+
+            while not done:
+                step_start = time.time()
+                state = data.qpos
+                action= agent.sample_action(state)
+                data.ctrl[0] = action
+                mujoco.mj_step(model, data)
+
+                ######  Calculate the Reward. #######
+                reward = 1.0
+                ###### End. The same as the official model. ########
+
+                rewards.append(reward)
+                states.append(state.copy())
+                done = data.time > 45 or abs(data.qpos[1]) > 0.18  # Example condition to end episode
+
+                ####################################
+                ### commit the following line to speed up the training, this will not show the simulation vedio frame.
+                ####################################
+                with viewer.lock():
+                    viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(data.time % 2)
+                viewer.sync()
+                
+                time_until_next_step = model.opt.timestep - (time.time() - step_start)
+                if time_until_next_step > 0:
+                    time.sleep(time_until_next_step)
+                ####################################
+                ####################################
+
+            agent.update(rewards, log_probs)
+            episode += 1
+
